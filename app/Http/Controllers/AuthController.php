aaaf6b67ae9747a2082f1618596cc1a4282e75b5
Ilya Khrebtov (mrProger) <zkhrebtovilya@gmail.com>
2022-05-21 01:36:34 +0600

    Добавлены валидации | Пофикшено исключение, когда не находило $user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function postLogin(Request $request) {
+        $result = 'false';
+        $validated = $this->validate($request, [
+            'login' => 'required|min:8|max:20',
+            'password' => 'required|min:8|max:20',
+        ]);
+
         $userData = array(
             'login' => $request->input('login'),
             'password' => $request->input('password')
@@ -18,26 +24,30 @@ class AuthController extends Controller
         }
 
         $account = User::where('login', $request->input('login'))->first();
-        $user = array(
-            'login' => $account->login,
-            'password' => $account->password,
-            'email' => $account->email,
-            'email_confirmed' => $account->email_confirmed,
-            'telephone' => $account->telephone,
-            'gender' => $account->gender
-        );
 
-        if (ControllerLogic::isNull($user) || !ControllerLogic::isCorrectPassword($user['password'], $request->input('password'))) {
-            return "false";
+        if ($account != null) {
+            $user = array(
+                'login' => $account->login,
+                'password' => $account->password,
+                'email' => $account->email,
+                'email_confirmed' => $account->email_confirmed,
+                'telephone' => $account->telephone,
+                'gender' => $account->gender
+            );
+
+            if (ControllerLogic::isNull($user) || !ControllerLogic::isCorrectPassword($user['password'], $request->input('password'))) {
+                return "false";
+            }
+
+            $result = $user['login'] . "\n"
+                . $user['password'] . "\n"
+                . $user['email'] . "\n"
+                . $user['email_confirmed'] . "\n"
+                . $user['telephone'] . "\n"
+                . $user['gender'];
         }
 
-        return $user['login'] . "\n"
-            . $user['password'] . "\n"
-            . $user['email'] . "\n"
-            . $user['email_confirmed'] . "\n"
-            . $user['telephone'] . "\n"
-            . $user['gender'];
-        //return "true";
+        return $result;
     }
 
     public function postRegister(Request $request) {
@@ -52,7 +62,11 @@ class AuthController extends Controller
             return "false";
         }
 
-        // Написать валидацию для логина и пароля
+        $validated = $this->validate($request, [
+            'login' => 'required|nullable|min:8|max:20',
+            'password' => 'required|nullable|min:8|max:20',
+            'email' => 'required|nullable|email',
+        ]);
 
         $user = new User();
         $user->login = $userData['login'];
